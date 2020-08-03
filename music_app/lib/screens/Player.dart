aaa9eea4d825ffdd6d027f241b38241e2b25bbaf
Thiago Dantas <thiago_dantas411@hotmail.com>
2020-08-03 01:13:57 -0300

    commit player ui
    - adicionado o player de musica na tela Player

--- music_app/lib/screens/Player.dart
+++ music_app/lib/screens/Player.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class Player extends StatefulWidget {
@@ -6,31 +8,163 @@ class Player extends StatefulWidget {
 }
 
 class _PlayerState extends State<Player> {
+  double valueSlide = 130;
+
   @override
   Widget build(BuildContext context) {
-
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Stack(
-        children: <Widget>[          
+        children: <Widget>[
           Container(
-            height: height,
-            width: width,
-            child: Image.asset("assets/capa_6.jpg",fit: BoxFit.cover,)
-          ),
-          Positioned(
-            top: 30,
-            left: 0,
-            right: 0,
-            child: _appBar()
-          ),
-
+              height: height,
+              width: width,
+              child: Image.asset(
+                "assets/capa_5.jpg",
+                fit: BoxFit.cover,
+              )),
+          Positioned(top: 30, left: 0, right: 0, child: _appBar()),
+          Positioned(bottom: 20, left: 0, right: 0, child: _player())
         ],
       ),
     );
   }
-  Widget _appBar(){
+
+  Widget _player() {
+    return Padding(
+      padding: EdgeInsets.only(left: 20, right: 20),
+      child: ClipRect(
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 4, sigmaY: 4),
+          child: Container(
+            height: 220,
+            decoration: BoxDecoration(
+                color: Colors.white10, borderRadius: BorderRadius.circular(17)),
+            padding: EdgeInsets.only(left: 20,right: 20,top: 20,bottom: 20),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "like it doens't hurt",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 1),
+                        ),
+                        SizedBox(
+                          height: 3,
+                        ),
+                        Text(
+                          "chris shelet",
+                          style: TextStyle(
+                              color: Colors.white30,
+                              letterSpacing: 1,
+                              fontSize: 12),
+                        ),
+                      ],
+                    ),
+                    Icon(
+                      Icons.add,
+                      color: Colors.white30,
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                
+                Flexible(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    //color: Colors.blue,
+                    height: 10,
+                    child: SliderTheme(
+                      data: SliderTheme.of(context).copyWith(
+                          thumbShape:
+                              RoundSliderThumbShape(enabledThumbRadius: 0.0)),
+                      child: Slider(
+                          activeColor: Color(0xffFF2842),
+                          inactiveColor: Colors.white30,
+                          min: 0,
+                          max: 245,
+                          value: valueSlide,
+                          onChanged: (value) {
+                            setState(() {
+                              valueSlide = value;
+                            });
+                          }),
+                    ),
+                  ),
+                ),                
+                Padding(
+                  padding: EdgeInsets.only(left: 23, right: 23),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        "01:30",
+                        style: TextStyle(color: Colors.white24, fontSize: 8),
+                      ),
+                      Text(
+                        "02:45",
+                        style: TextStyle(color: Colors.white24, fontSize: 8),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 15,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.skip_previous, color: Colors.white,), 
+                      onPressed: (){}
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.fast_rewind, color: Colors.white,), 
+                      onPressed: (){}
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.pause, color: Colors.white,), 
+                      onPressed: (){}
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.fast_forward, color: Colors.white,), 
+                      onPressed: (){}
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.skip_next, color: Colors.white,), 
+                      onPressed: (){}
+                    ),
+                  ],
+                ),
+                SizedBox(height: 15,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Icon(Icons.refresh, color: Colors.white30,), 
+                     
+                    Icon(Icons.swap_horiz, color: Colors.white30,), 
+                      
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _appBar() {
     return Container(
       padding: EdgeInsets.only(left: 10, right: 10, top: 0),
       child: Row(
@@ -38,7 +172,7 @@ class _PlayerState extends State<Player> {
         children: <Widget>[
           IconButton(
             onPressed: () => Navigator.pop(context),
-            icon:Icon(
+            icon: Icon(
               Icons.arrow_back,
               color: Colors.white70,
             ),
