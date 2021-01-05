e879e3c4c582906f6345593ee7ec3e55f0eb13d4
Thiago Dantas <thiago_dantas411@hotmail.com>
2021-01-05 11:37:07 -0300

    commit social app implement postdetail
    - implement view postDetail(initial)

--- social_app/lib/views/post_detail/post_detail.dart
+++ social_app/lib/views/post_detail/post_detail.dart
@@ -10,7 +10,46 @@ class PostDetail extends StatelessWidget {
         backgroundColor: Colors.transparent,
       ),
 
-      body: Container(),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width:MediaQuery.of(context).size.width ,
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.network('https://photos8.com/wp-content/uploads/2019/11/Love-Hands-Heart-Copyright-by-Sam-Mugraby.jpg',
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height/2,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(height: 20,),
+              Padding(
+                padding: EdgeInsets.only(left: 15,right: 15),
+                child: Row(
+                  children: [
+                    IconButton(
+                      icon: Icon(FontAwesomeIcons.comment,color: Colors.white,), 
+                      onPressed: (){}
+                    ),
+                    IconButton(
+                      icon: Icon(FontAwesomeIcons.heart,color: Colors.white, ), 
+                      onPressed: (){}
+                    ),
+                    /*IconButton(
+                      icon: Icon(FontAwesomeIcons.solidHeart,color: Colors.white, ), 
+                      onPressed: (){}
+                    ),*/
+                    IconButton(
+                      icon: Icon(FontAwesomeIcons.shareAlt,color: Colors.white, ), 
+                      onPressed: (){}
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
